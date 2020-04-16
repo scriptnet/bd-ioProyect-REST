6a3f5cb1a85659d2b91eeee18a35033ca8398972
scriptnet <alex147f1@gmail.com>
2020-04-16 15:15:06 -0500

    panel diseño

--- app/static/app.js
+++ app/static/app.js
@@ -1,11 +1,24 @@
 ! function(a){
+    "use strict";
+
+    function c(a,b){
+       b.otherwise('/'),
+       a.state("dashboard",{
+           url: "/",
+           templateUrl: "static/views/dashboard/dashboard.html",
+       });
+    }
 
     function b(a) {
         a.title = "Hola mundo";
-        
     }
-    a.module("app", [])
+    a.module("app", ["ui.router", "ngSanitize"])
+    .config(c)
+    .component("mainNav",{
+        templateUrl: "static/views/main-nav/main-nav.html",
+    })
     .controller("appController", b);
-
+    c.$inject = ["$stateProvider", "$urlRouterProvider"];
     b.$inject = ["$scope"];
+
 }(angular)
